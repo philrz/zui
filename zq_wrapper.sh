346e8e5666540cdbd3d68b6f46f47f9edc8ed99a
Phil Rzewski <phil@brimdata.io>
2024-02-28 15:29:24 -0800

    Add zq wrapper

---
+++ zq_wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TMP_STDOUT=$(mktemp)_stdout.txt
+TMP_STDERR=$(mktemp)_stderr.txt
+TMP_CALLED_WITH=$(mktemp)
+echo "I was called with: $@" > ${TMP_CALLED_WITH}_zq_wrapper_called_with.txt
+zq "$@" > $TMP_STDOUT 2> $TMP_STDERR
+cat $TMP_STDOUT
+cat $TMP_STDERR >&2
+#rm -f $TMP_STDOUT $TMP_STDERR
