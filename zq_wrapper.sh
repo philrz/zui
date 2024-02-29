4e67ac39e513365269cb61075e3a5fb97cc09814
Phil Rzewski <phil@brimdata.io>
2024-02-29 13:18:39 -0800

    Have wrapper script return zq's exit code

--- zq_wrapper.sh
+++ zq_wrapper.sh
@@ -4,5 +4,7 @@ TMP_STDERR=$(mktemp)_zq_wrapper_stderr.txt
 TMP_CALLED_WITH=$(mktemp)
 echo "I was called with: $@" > ${TMP_CALLED_WITH}_zq_wrapper_called_with.txt
 ../../apps/zui/zdeps/zq "$@" > $TMP_STDOUT 2> $TMP_STDERR
+ERR="$?"
 cat $TMP_STDOUT
 cat $TMP_STDERR >&2
+exit $ERR
